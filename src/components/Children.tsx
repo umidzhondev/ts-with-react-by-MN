1d15bc242b196303fbbd983c085678f667f1312f
umidzhondev <umidzhonfullstackdev@gmail.com>
2022-11-01 23:49:46 +0600

    Lesson-49: Named and Default Exports

---
+++ src/components/Children.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Children = () => {
+  return (
+    <div>Children</div>
+  )
+}
+
+export const Children2 =  () =>{
+    return(
+        <div>Children2</div>
+    )
+}
---
+++ src/components/Parents.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Children,Children2} from './Children'
+
+const Parents = () => {
+  return (
+    <>
+        <Children/>
+        <Children2/>
+    </>
+  )
+}
+
+export default Parents
